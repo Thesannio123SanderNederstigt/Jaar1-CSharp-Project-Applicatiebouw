bdcc5f2d4dac7d2fc8bd18ce17d6084a3c480624
gillian1998 <54937872+gillian1998@users.noreply.github.com>
2020-05-13 16:39:57 +0200

    tt

---
+++ ChapooApllication/ChapooModel/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapooModel
+{
+    public class MenuItem
+    {
+        public int ID { get; set; }
+        public int menukaartID { get; set; }
+        public string omschrijving { get; set; }
+    }
+}
---
+++ ChapooApllication/ChapooModel/Tafel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapooModel
+{
+    public class Tafel
+    {
+        public int ID { get; set; }
+        public int status { get; set; }
+        public int medewerkerID { get; set; }
+    }
+}
