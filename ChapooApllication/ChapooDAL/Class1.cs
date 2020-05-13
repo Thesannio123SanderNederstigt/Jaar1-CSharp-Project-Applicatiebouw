a1b75c5484a853b87809d45b7cec899559c58510
Mathijs <54897766+Vlietuig@users.noreply.github.com>
2020-05-13 14:03:51 +0200

    Added References
    tussen de classes/projects

--- ChapooApllication/ChapooDAL/Class1.cs
+++ ChapooApllication/ChapooDAL/Class1.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ChapooModel;
+using System.Data;
+using System.Data.SqlClient;
+
 namespace ChapooDAL
 {
     public class Class1
--- ChapooApllication/ChapooLogic/Class1.cs
+++ ChapooApllication/ChapooLogic/Class1.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ChapooDAL;
+using ChapooModel;
+
 namespace ChapooLogic
 {
     public class Class1
     {
+
     }
 }
