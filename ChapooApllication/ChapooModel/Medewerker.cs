f7005023d58b3efef9351f8652f5319622d3dc23
Thesannio123SanderNederstigt <thesannio123@gmail.com>
2020-05-20 10:35:12 +0200

    medewerkers
    medewerker, medewerkerhandler en medewerkerDAO eerste aanpassingen

--- ChapooApllication/ChapooModel/Medewerker.cs
+++ ChapooApllication/ChapooModel/Medewerker.cs
@@ -8,11 +8,21 @@ namespace ChapooModel
 {
     public class Medewerker
     {
-        public int ID { get; set; }
+        public int id { get; set; }
         public string voornaam { get; set; }
         public string achternaam { get; set; }
         public string type { get; set; }
         public int inlogcode { get; set; }
         public int werkstationID { get; set; }
+
+        public Medewerker(int id, string voornaam, string achternaam, string type, int inlogcode, int werkstationid)
+        {
+            this.id = id;
+            this.voornaam = voornaam;
+            this.achternaam = achternaam;
+            this.type = type;
+            this.inlogcode = inlogcode;
+            this.werkstationID = werkstationid;
+        }
     }
 }
